05ca3217a954def87d39b8d106e64d29aaeb4e1a
gregt101 <gregt101@gmail.com>
2014-03-04 01:53:35 -0800

    Update Book.java

--- src/main/java/edu/sjsu/cmpe/library/domain/Book.java
+++ src/main/java/edu/sjsu/cmpe/library/domain/Book.java
@@ -1,9 +1,19 @@
 package edu.sjsu.cmpe.library.domain;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.NotEmpty;
 
 
 public class Book {
     private long isbn;
+    @NotNull(message = "title is required! ")
     private String title;
+    @JsonProperty("publication-date")
+    @NotNull(message = "publication date is requied! ")
+    private String pubDate;
+    private String language = "";
+    @JsonProperty("num-pages")
+    private int numPage = 0;
+    private String status = "available";
 
     // add more fields here
 
@@ -36,4 +46,54 @@ public class Book {
     public void setTitle(String title) {
 	this.title = title;
     }
+    
+    public String getPubdate() {
+	return pubDate;
+    }
+
+    public void setPubdate(String pubDate) {
+	this.pubDate = pubDate;
+    }
+    
+    public String getLanguage() {
+	return language;
+    }
+
+    public void setLanguage(String language) {
+	this.language = language;
+    }
+    
+    public String getLanguage() {
+	return language;
+    }
+
+    public void setLanguage(String language) {
+	this.language = language.toLowerCase();
+    }
+    
+    public int getNumpage() {
+	return numPage;
+    }
+
+    public void setNumpage(int numPage) {
+	this.numPage = numPage;
+    }
+    
+    public String getStatus() {
+	return status;
+    }
+
+    public void setStatus(String status) {
+    	if((status.toLowerCase().contains("available")||
+    	   (status.toLowerCase().contains("checked-out")||
+    	   (status.toLowerCase().contains("in-queue")||
+    	   (status.toLowerCase().contains("lost"))
+		this.status = status.toLowerCase();
+	else if status==""
+	        this.status = "available";
+	else
+		this.status = "error";
+	
+    }
+    
 }
