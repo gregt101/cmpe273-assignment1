0e476e3e4ac8069f612495b0aed76ca2afa69977
Ubuntu <ubuntu@ip-172-31-20-214.us-west-1.compute.internal>
2014-03-09 04:00:11 +0000

    add for returning reviews

---
+++ src/main/java/edu/sjsu/cmpe/library/dto/ReviewsDto.java
@@ -0,0 +1,39 @@
+package edu.sjsu.cmpe.library.dto;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import edu.sjsu.cmpe.library.domain.Review;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+//@JsonPropertyOrder(alphabetic = true)
+@JsonPropertyOrder({"reviews" ,"links"})
+public class ReviewsDto extends LinksDto {
+    private List<Review> reviews;
+
+    /**
+     * @param book
+     */
+    public ReviewsDto(ArrayList<Review> reviewArray) {
+	super();
+	this.reviews = reviewArray;
+    }
+
+    /**
+     * @return the review
+     */
+    public List<Review> getReviews() {
+	return reviews;
+    }
+
+    /**
+     * @param review
+     *            the review to set
+     */
+    public void setReviews(ArrayList<Review> reviewArray) {
+	this.review = review;
+    }
+    
+}
